b4b387a60a264a956b72335e75f47e635eaa9111
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-04 00:31:14 +0100

    update routing

--- rottentomatoes/urls.py
+++ rottentomatoes/urls.py
@@ -13,9 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from reviews import views as reviews_views
+from movies import views as movies_views
+from users import views as users_views
+from .views import index
+
+router = routers.SimpleRouter()
+router.register(r'reviews', reviews_views.ReviewModelViewSet)
+router.register(r'movies', movies_views.MovieModelViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('api/', include((router.urls, 'api'))),
+    path('api/login/', users_views.Login.as_view()),
+    path('api/logout/', users_views.Logout.as_view()),
+    path('api/register/', users_views.CreateUserView.as_view()),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
