ba5243b291f97b47898b1efec58b7b2bd4989f45
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-03 17:19:01 +0100

    WIP create models and register to admin area

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    synopsis = models.TextField()
+    slug = models.SlugField(null=False, blank=False, unique=True)
+    image = models.ImageField(upload_to='movies/', null=False, blank=False)
+    release_date = models.DateTimeField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+def set_slug(sender, instance, *args, **kwargs):  # callback pre save
+    if instance.title and not instance.slug:
+        slug = slugify(instance.title)
+
+        while Movie.objects.filter(slug=slug).exists():
+            slug = slugify(
+                f'{instance.title}-{str(uuid.uuid4())[:8]}'
+            )
+        instance.slug = slug
+
+
+pre_save.connect(set_slug, sender=Product)
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from movies.models import Movie
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Review(models.Model):
+    title = models.TextField(max_length=100)
+    comment = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    stars = models.PositiveIntegerField(max_length=5, default=0)
+
+    movie = models.ForeignKey(Movie, null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
