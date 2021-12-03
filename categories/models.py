c81a750d61245e27256a67328d6036fd9828a039
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-04 00:31:39 +0100

    DB stuff

--- categories/models.py
+++ categories/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    movies = models.ManyToManyField(Movie, blank=True)
+    # movies = models.ManyToOneRel(Movie)
 
     def __str__(self):
         return self.title
