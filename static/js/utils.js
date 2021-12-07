aa428e04b0f4e32d799fea9e591e723961f76853
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-07 23:24:25 +0100

    complete login

---
+++ static/js/utils.js
@@ -0,0 +1,37 @@
+function getFormData(form_id) {
+    return Array.from(document.querySelectorAll(`#${form_id} input`)).reduce((acc, input) => ({ ...acc, [input.id]: input.value}),{});
+}
+
+function setCookie(cname, cvalue, exdays=1) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
