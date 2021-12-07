25f0995080449013e93e5eec2c2e4a1643afa67c
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-07 03:50:40 +0100

    improve movie filters

--- movies/views.py
+++ movies/views.py
@@ -1,24 +1,29 @@
-from django.shortcuts import render
-
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
-from .serializers import MovieModelSerielizer
+
 from .models import Movie
+from .serializers import MovieModelSerielizer
+
 
 class MovieModelViewSet(ModelViewSet):
     serializer_class = MovieModelSerielizer
     queryset = Movie.objects.all()
 
     def get_queryset(self):
-        queryset = Movie.objects.all()
+        filters_list = []
 
         # Search by title
         title = self.request.query_params.get('title')
         if title is not None:
-            queryset = Movie.objects.filter(title__icontains=title)
+            filters_list.append(Q(title__icontains=title))
 
-        # Search by Category
+        # Search by Category
         category_title = self.request.query_params.get('category')
         if category_title is not None:
-            queryset = Movie.objects.filter(category__title__icontains=category_title)
+            filters_list.append(Q(category__title__icontains=category_title))
 
-        return queryset
+        filters = Q()
+        for filter in filters_list:
+            filters = filters.__and__(filter)
+
+        return Movie.objects.filter(filters)
