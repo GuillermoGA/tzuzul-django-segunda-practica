1ae60758f88e10ddf45ee3733a4b256b4f6241dd
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-07 03:50:03 +0100

    get category from movie details

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
+
+from categories.serializers import CategoryModelSerializer
+from reviews.serializers import ReviewModelSerielizer
 from .models import Movie
 
-from reviews.serializers import ReviewModelSerielizer
 
 class MovieModelSerielizer(serializers.ModelSerializer):
     reviews = ReviewModelSerielizer(read_only=True, many=True)
+    category = CategoryModelSerializer(read_only=True)
+
     class Meta:
         model = Movie
-        fields = ["id", "title", "synopsis", "release_date", "image", "reviews"]
+        fields = ["id", "title", "synopsis", "release_date", "image", "reviews", "category"]
